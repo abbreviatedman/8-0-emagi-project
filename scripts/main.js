f23cb4206e4bb4dc68c782310490c9a872e45ac2
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-10-05 10:49:37 -0400

    search-section

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  fetch("https://emagi-server-8-0.herokuapp.com/search/${term}")
+    .then((response) => response.json())
+    .then((emojis) => {
+      const term = event.target.search.value;
+      const result = search(term, emojis)
+        .map((emoji) => emoji.symbol)
+        .join("");
+      const resultArea = document.querySelector("#search aside p");
+      resultArea.textContent = result;
+      document.querySelector("#search aside").classList.add("success");
+    });
+});
