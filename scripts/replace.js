44c6815836d97e3707615279a5ec2b815c9784dc
salrey <tekdehn@gmail.com>
2021-10-05 16:15:02 -0400

    Update replace function to replace partial words and words with punctuation

--- scripts/replace.js
+++ scripts/replace.js
@@ -7,9 +7,10 @@ const replace = (text, emojis) => {
     // const rightEmoji = emojis.filter((emoji) => emoji.name === word.toLowerCase())
     // if (rightEmoji.length) {
     //   word = rightEmoji[0].symbol
-    const found = emojis.find((emoji) => emoji.name === word.toLowerCase());
+    // const found = emojis.find((emoji) => emoji.name === word.toLowerCase());
+    const found = emojis.find((emoji) => word.toLowerCase().includes(emoji.name));
     if (found) {
-      word = found.symbol;
+      word = word.toLowerCase().replace(found.name, found.symbol);
     }
     return word
   })
