268b1bff25491d265af1c9bfa562ba5426997aae
DylanLovell <arcanyne7@gmail.com>
2021-10-06 12:46:28 -0400

    funny project

--- scripts/encode.js
+++ scripts/encode.js
@@ -1,14 +1,22 @@
 // You encode function should go here.
-
+const encode = (text, emojis) => {
+  return text
+    .split('')
+    .map((char) => {
+      let result = emojis.find((emoji) => emoji.letter === char.toLowerCase());
+      return result ? result.symbol : char;
+    })
+    .join('');
+};
 // DON'T TOUCH BELOW!
 // If you haven't made the function yet, this check makes sure other tests can still run.
-if (typeof encode === "undefined") {
+if (typeof encode === 'undefined') {
   encode = undefined;
 }
 
 // This check makes sure to export the file only if we're running tests in the terminal.
 // If you try to use `module.exports` in the browser, you'll get a logged error.
 // It won't break anything, but it's easy to confuse it with a serious error.
-if (typeof module !== "undefined") {
+if (typeof module !== 'undefined') {
   module.exports = encode;
 }
--- scripts/get-category.js
+++ scripts/get-category.js
@@ -1,14 +1,15 @@
 // Your `getCategory` function should go here.
-
+const getCategory = (cat, emojis) =>
+  emojis.filter((emoji) => emoji.category === cat);
 // DON'T TOUCH BELOW!
 // If you haven't made the function yet, this check makes sure other tests can still run.
-if (typeof getCategory === "undefined") {
+if (typeof getCategory === 'undefined') {
   getCategory = undefined;
 }
 
 // This check makes sure to export the file only if we're running tests in the terminal.
 // If you try to use `module.exports` in the browser, you'll get a logged error.
 // It won't break anything, but it's easy to confuse it with a serious error.
-if (typeof module !== "undefined") {
+if (typeof module !== 'undefined') {
   module.exports = getCategory;
 }
