4637a2775ebff646dad1a29a02b6de63af50e2a3
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-06 11:53:53 -0400

    Added a promise for encode function in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,16 @@ document.querySelector("#search form").addEventListener("submit", (event) => {
             document.querySelector("#search aside").classList.add("success");
         })
 })
+
+document.querySelector("#encode form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    fetch(`https://emagi-server-8-0.herokuapp.com/emojis`)
+    .then((response) => response.json())
+    .then((emojis) => {
+            const term = event.target.search.value;
+            const result = encode(term, emojis);
+            const resultArea = document.querySelector("#encode aside p");
+            resultArea.textContent = result;
+            document.querySelector("#encode aside").classList.add("success");
+        })
+})
