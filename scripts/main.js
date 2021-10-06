ce95c6ff9eb9299bc29c4e4d2fc523d164fbaef1
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-10-06 12:56:08 -0400

    added replace

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ document.querySelector("#search form").addEventListener("submit", (event) => {
 event.preventDefault(); //prevents reload
 const term = event.target.search.value;
 fetch(baseUrl + `/search/${term}`)
-.then((response) => response.json())
+.then((response) => response.json()) //
 .then((emojis) => {
 const result = emojis.map((emoji) => emoji.symbol).join("");
 const resultArea = document.querySelector("#search aside p")
@@ -24,5 +24,18 @@ document.querySelector("#encode form").addEventListener("submit", (event) => {
     const resultArea = document.querySelector("#encode aside p")
     resultArea.textContent = result;
     document.querySelector("#encode aside").classList.add("success");
+    }) 
+})
+
+document.querySelector("#replace form").addEventListener("submit", (event) => {
+    event.preventDefault(); //prevents reload
+    fetch(baseUrl + `/emojis`)
+    .then((response) => response.json())
+    .then((emojis) => {
+    const term = event.target.replace.value;
+    const result = replace(term, emojis);
+    const resultArea = document.querySelector("#replace aside p")
+    resultArea.textContent = result;
+    document.querySelector("#replace aside").classList.add("success");
     }) 
 })
