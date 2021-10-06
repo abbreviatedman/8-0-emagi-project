e18e74cca7a6113c5ac01a4b0a80d1b0087d96b9
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-10-06 11:51:50 -0400

    added encoded.

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+const baseUrl = "https://emagi-server-8-0.herokuapp.com";
+
+//search//
+document.querySelector("#search form").addEventListener("submit", (event) => {
+event.preventDefault(); //prevents reload
+const term = event.target.search.value;
+fetch(baseUrl + `/search/${term}`)
+.then((response) => response.json())
+.then((emojis) => {
+const result = emojis.map((emoji) => emoji.symbol).join("");
+const resultArea = document.querySelector("#search aside p")
+resultArea.textContent = result;
+document.querySelector("#search aside").classList.add("success");
+}) 
+})    
+//encode//
+document.querySelector("#encode form").addEventListener("submit", (event) => {
+    event.preventDefault(); //prevents reload
+    fetch(baseUrl + `/emojis`)
+    .then((response) => response.json())
+    .then((emojis) => {
+    const term = event.target.search.value;
+    const result = encode(term, emojis);
+    const resultArea = document.querySelector("#encode aside p")
+    resultArea.textContent = result;
+    document.querySelector("#encode aside").classList.add("success");
+    }) 
+})
